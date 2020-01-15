b72e7158f8ecfcc885ed42ac378fccac07c64cf6
Dendi Maniac <daq30899@gmail.com>
2020-01-15 12:14:11 +0200

    finished task a, unfixed bug

--- components/List.js
+++ components/List.js
@@ -1,19 +1,22 @@
 import { FlatList } from 'react-native';
 import ListItem from './ListItem';
-import React from 'react';
-import { PropTypes } from 'prop-types';
+import React, { useContext } from 'react';
+import { MediaContext } from '../contexts/MediaContext';
+import { useFetch } from '../hooks/APIHooks';
 
-const List = ({data}) => {
+const List = () => {
+  const [media, setMedia] = useContext(MediaContext);
+  const [data, loading] = useFetch("https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json");
+  console.log(data, loading);
+  setMedia(data);
+
   return (
     <FlatList
-      data={data}
+      data={media}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item}/>}
     />
   );
-};
-
-List.propTypes = {
-  data: PropTypes.array,
 };
 
 export default List;
